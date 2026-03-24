22822faa5e7bfa77a3ca33ec0ef10f2cba5e64fa
emmanuelsimiyum24-cell <emmanuelsimiyum24@gmail.com>
2026-03-24 22:53:35 +0300

    level one complete
    Level one complete

--- login.cpp
+++ login.cpp
@@ -2,22 +2,30 @@
 using namespace std;
 
 int main() {
-    // Hardcoded credentials
-    string correctUsername = "adminKE";
-    string correctPassword = "254Secure";
+    string correctUsername = "EMMANUEL";
+    string correctPassword = "MANU254";
 
     string username, password;
 
-    // User input
     cout << "Enter username: ";
     cin >> username;
 
     cout << "Enter password: ";
     cin >> password;
 
-    // Validation
+    
     if (username == correctUsername && password == correctPassword) {
         cout << "Access Granted" << endl;
+    } else {
+        cout << "Invalid Credentials" << endl;
+    }
+
+    
+    cout << "\n--- Logical Error Demo ---" << endl;
+
+    
+    if (username == correctUsername || password == correctPassword) {
+        cout << "Access Granted (WRONG LOGIC)" << endl;
     } else {
         cout << "Invalid Credentials" << endl;
     }
--- login.py
+++ login.py
@@ -1,13 +1,18 @@
-# Hardcoded credentials
-correct_username = "adminKE"
-correct_password = "254Secure"
+# ===== CORRECT VERSION =====
+correct_username = "EMMANUEL"
+correct_password = "Manu123"
 
-# User input
 username = input("Enter username: ")
 password = input("Enter password: ")
 
-# Validation
 if username == correct_username and password == correct_password:
     print("Access Granted")
+else:
+    print("Invalid Credentials")
+
+print("\n--- Logical Error Demo ---")
+
+if username == correct_username or password == correct_password:
+    print("Access Granted (WRONG LOGIC)")
 else:
     print("Invalid Credentials")
